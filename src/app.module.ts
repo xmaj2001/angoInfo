6334538db9f1c7b21ad72c848211112df990ec3b
xmaj2001 <xmaj2001@gmail.com>
2025-07-01 09:46:40 +0100

    wq

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,5 +2,6 @@ import { Module } from '@nestjs/common';
 import { MunicipioModule } from './modules/municipio/provincia.module';
 @Module({
   imports: [MunicipioModule],
+  providers: [],
 })
 export class AppModule {}
--- src/modules/municipio/provincia.module.ts
+++ src/modules/municipio/provincia.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MunicipioService } from './municipio.service';
 import { MunicipioController } from './provincia.controller';
-import { DatabaseService } from '../../database/database.service';
 
 @Module({
   controllers: [MunicipioController],
-  providers: [MunicipioService, DatabaseService],
+  providers: [MunicipioService],
 })
 export class MunicipioModule {}
